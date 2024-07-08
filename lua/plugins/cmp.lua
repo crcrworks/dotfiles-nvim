2ef4897d2fbc4578366d6d0c1b0459f9fe5b51b5
crcrworks <42961885+crcrworks@users.noreply.github.com>
2024-07-09 00:57:43 +0900

    add cmp completion

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,14 +1,33 @@
-local cmp = require "cmp"
-local border_opts = {
-  border = "none",
-  winhighlight = "Normal:StatusLineNC,FloatBorder:FloatBorder,CursorLine:FloatShadow,Search:None",
-}
 return {
   "hrsh7th/nvim-cmp",
-  opts = {
-    window = {
+  opts = function(_, opts)
+    local cmp = require "cmp"
+    local border_opts = {
+      border = "none",
+      winhighlight = "Normal:StatusLineNC,FloatBorder:FloatBorder,CursorLine:FloatShadow,Search:None",
+    }
+
+    vim.opt.spell = true
+    vim.opt.spelllang = { "en_us" }
+
+    opts.window = {
       completion = cmp.config.window.bordered(border_opts),
       documentation = cmp.config.window.bordered(border_opts),
-    },
-  },
+    }
+    opts.sources = cmp.config.sources {
+      { name = "nvim_lsp" },
+      { name = "buffer" },
+      { name = "path" },
+      { name = "treesitter" },
+      { name = "copilot", group_index = 2 },
+      {
+        name = "spell",
+        option = {
+          keep_all_entries = false,
+          enable_in_context = function() return true end,
+          preselect_correct_word = true,
+        },
+      },
+    }
+  end,
 }
