8ca39d146c4a16c9e68db7b560b8e6d5ea4c35d6
crcrworks <42961885+crcrworks@users.noreply.github.com>
2024-09-02 20:40:00 +0900

    add snippets for react

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,10 +1,7 @@
 return {
   "L3MON4D3/LuaSnip",
   config = function(plugin, opts)
-    require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-    -- add more custom luasnip configuration such as filetype extend or custom snippets
-    local luasnip = require "luasnip"
-    luasnip.filetype_extend("javascript", { "javascriptreact" })
+    require "astronvim.plugins.configs.luasnip"(plugin, opts)
 
     require("luasnip.loaders.from_vscode").lazy_load {
       paths = { vim.fn.stdpath "config" .. "/snippets" },
