95252b35764b3be9f89ca44da0a97efa016265ec
crcrworks <42961885+crcrworks@users.noreply.github.com>
2024-09-02 20:40:11 +0900

    config telescope for unity

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,9 +3,17 @@ return {
   opts = function(_, opts)
     opts.defaults.file_ignore_patterns = {
       ".meta",
+      ".asset",
       ".prefab",
       ".png",
       ".pdf",
+      ".mat",
+      ".conf",
+      ".asmdef",
+      ".*lock*",
+      "ProjectSettings/*",
+      "Packages/*",
+      "TextMesh Pro/*",
     }
   end,
 }
