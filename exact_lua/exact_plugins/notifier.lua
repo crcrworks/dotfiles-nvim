9d6e0b704fe43fcf68a800d8fe80c633324c44dd
crcrworks <42961885+crcrworks@users.noreply.github.com>
2025-03-28 13:18:53 +0900

    delete notifier types

--- exact_lua/exact_plugins/notifier.lua
+++ exact_lua/exact_plugins/notifier.lua
@@ -1,14 +1,10 @@
 return {
-  "folke/snacks.nvim",
-  opts = {
-    ---@class snacks.notifier.Config
-    ---@field enabled? boolean
-    ---@field keep? fun(notif: snacks.notifier.Notif): boolean # global keep function
-    ---@field filter? fun(notif: snacks.notifier.Notif): boolean # filter our unwanted notifications (return false to hide)
-    notifier = {
-      style = "minimal",
-      padding = false,
-      top_down = false,
-    },
-  },
+	"folke/snacks.nvim",
+	opts = {
+		notifier = {
+			style = "minimal",
+			padding = false,
+			top_down = false,
+		},
+	},
 }
