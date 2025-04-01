806905361400bffcdafd6e6da75714f8b72c59f6
crcrworks <42961885+crcrworks@users.noreply.github.com>
2025-04-01 23:55:16 +0900

    update polish

--- exact_lua/polish.lua
+++ exact_lua/polish.lua
@@ -1,18 +1,31 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process and is a good place to configure
 -- things like custom filetypes. This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
+vim.cmd([[
+  xnoremap p "_dP
+  " nnoremap <C-c> <Esc>
+  " inoremap <C-c> <Esc>
+  " vnoremap <C-c> <Esc>
+  " cnoremap <C-c> <Esc>
+]])
+
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+	pattern = "*.sk",
+	callback = function()
+		vim.bo.filetype = "skript"
+	end,
+})
+
 -- Set up custom filetypes
-vim.filetype.add {
-  extension = {
-    foo = "fooscript",
-  },
-  filename = {
-    ["Foofile"] = "fooscript",
-  },
-  pattern = {
-    ["~/%.config/foo/.*"] = "fooscript",
-  },
-}
+vim.filetype.add({
+	extension = {
+		foo = "fooscript",
+	},
+	filename = {
+		["Foofile"] = "fooscript",
+	},
+	pattern = {
+		["~/%.config/foo/.*"] = "fooscript",
+	},
+})
