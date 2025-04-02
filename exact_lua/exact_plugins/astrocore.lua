562f8921b80175b7965d7662f920ccd8bc5ebfbf
crcrworks <42961885+crcrworks@users.noreply.github.com>
2025-04-03 00:45:53 +0900

    format astrocore

--- exact_lua/exact_plugins/astrocore.lua
+++ exact_lua/exact_plugins/astrocore.lua
@@ -5,90 +5,90 @@
 
 ---@type LazySpec
 return {
-  "AstroNvim/astrocore",
-  ---@type AstroCoreOpts
-  opts = {
-    -- Configure core features of AstroNvim
-    features = {
-      large_buf = { size = 1024 * 256, lines = 10000 },          -- set global limits for large files for disabling features like treesitter
-      autopairs = true,                                          -- enable autopairs at start
-      cmp = true,                                                -- enable completion at start
-      diagnostics = { virtual_text = true, virtual_lines = false }, -- diagnostic settings on startup
-      highlighturl = true,                                       -- highlight URLs at start
-      notifications = true,                                      -- enable notifications at start
-    },
-    -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
-    diagnostics = {
-      virtual_text = true,
-      underline = true,
-    },
-    -- vim options can be configured here
-    options = {
-      opt = {               -- vim.opt.<key>
-        relativenumber = false, -- sets vim.opt.relativenumber
-        number = true,      -- sets vim.opt.number
-        spell = false,      -- sets vim.opt.spell
-        signcolumn = "yes", -- sets vim.opt.signcolumn to yes
-        wrap = true,        -- sets vim.opt.wrap
-        conceallevel = 2,
-        numberwidth = 1,
-      },
-      g = { -- vim.g.<key>
-        -- configure global vim variables (vim.g)
-        -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
-        -- This can be found in the `lua/lazy_setup.lua` file
-      },
-    },
-    -- Mappings can be configured through AstroCore as well.
-    -- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
-    mappings = {
-      -- first key is the mode
-      n = {
-        ["<Tab>"] = {
-          function()
-            require("astrocore.buffer").nav(vim.v.count > 0 and vim.v.count or 1)
-          end,
-          desc = "Next buffer",
-        },
-        ["<S-Tab>"] = {
-          function()
-            require("astrocore.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1))
-          end,
-          desc = "Previous buffer",
-        },
+	"AstroNvim/astrocore",
+	---@type AstroCoreOpts
+	opts = {
+		-- Configure core features of AstroNvim
+		features = {
+			large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
+			autopairs = true, -- enable autopairs at start
+			cmp = true, -- enable completion at start
+			diagnostics = { virtual_text = true, virtual_lines = false }, -- diagnostic settings on startup
+			highlighturl = true, -- highlight URLs at start
+			notifications = true, -- enable notifications at start
+		},
+		-- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
+		diagnostics = {
+			virtual_text = true,
+			underline = true,
+		},
+		-- vim options can be configured here
+		options = {
+			opt = { -- vim.opt.<key>
+				relativenumber = false, -- sets vim.opt.relativenumber
+				number = true, -- sets vim.opt.number
+				spell = false, -- sets vim.opt.spell
+				signcolumn = "yes", -- sets vim.opt.signcolumn to yes
+				wrap = true, -- sets vim.opt.wrap
+				conceallevel = 2,
+				numberwidth = 1,
+			},
+			g = { -- vim.g.<key>
+				-- configure global vim variables (vim.g)
+				-- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
+				-- This can be found in the `lua/lazy_setup.lua` file
+			},
+		},
+		-- Mappings can be configured through AstroCore as well.
+		-- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
+		mappings = {
+			-- first key is the mode
+			n = {
+				["<Tab>"] = {
+					function()
+						require("astrocore.buffer").nav(vim.v.count > 0 and vim.v.count or 1)
+					end,
+					desc = "Next buffer",
+				},
+				["<S-Tab>"] = {
+					function()
+						require("astrocore.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1))
+					end,
+					desc = "Previous buffer",
+				},
 
-        -- mappings seen under group name "Buffer"
-        ["<Leader>bd"] = {
-          function()
-            require("astroui.status.heirline").buffer_picker(function(bufnr)
-              require("astrocore.buffer").close(bufnr)
-            end)
-          end,
-          desc = "Close buffer from tabline",
-        },
+				-- mappings seen under group name "Buffer"
+				["<Leader>bd"] = {
+					function()
+						require("astroui.status.heirline").buffer_picker(function(bufnr)
+							require("astrocore.buffer").close(bufnr)
+						end)
+					end,
+					desc = "Close buffer from tabline",
+				},
 
-        ["<Leader>bD"] = {
-          function()
-            require("astroui.status.heirline").buffer_picker(function(bufnr)
-              require("astrocore.buffer").close(bufnr)
-            end)
-          end,
-          desc = "Pick to close",
-        },
+				["<Leader>bD"] = {
+					function()
+						require("astroui.status.heirline").buffer_picker(function(bufnr)
+							require("astrocore.buffer").close(bufnr)
+						end)
+					end,
+					desc = "Pick to close",
+				},
 
-        -- tables with just a `desc` key will be registered with which-key if it's installed
-        -- this is useful for naming menus
-        ["<Leader>b"] = { desc = "Buffers" },
-        ["<Leader>m"] = {
-          function()
-            require("astrocore.buffer").sort("modified")
-          end,
-          desc = "Sort buffers by modify",
-        },
+				-- tables with just a `desc` key will be registered with which-key if it's installed
+				-- this is useful for naming menus
+				["<Leader>b"] = { desc = "Buffers" },
+				["<Leader>m"] = {
+					function()
+						require("astrocore.buffer").sort("modified")
+					end,
+					desc = "Sort buffers by modify",
+				},
 
-        -- setting a mapping to false will disable it
-        -- ["<C-S>"] = false,
-      },
-    },
-  },
+				-- setting a mapping to false will disable it
+				-- ["<C-S>"] = false,
+			},
+		},
+	},
 }
